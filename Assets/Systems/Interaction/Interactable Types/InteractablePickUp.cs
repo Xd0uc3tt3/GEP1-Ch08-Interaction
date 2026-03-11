4fc6ace3e6e67f42004a3c40660711e02a97bcb5
Xd0uc3tt3 <145155890+Xd0uc3tt3@users.noreply.github.com>
2026-03-11 20:15:17 -0300

    Added Interactable Gem and Sign
    Sign interactable will get its own script later

--- Assets/Systems/Interaction/Interactable Types/InteractablePickUp.cs
+++ Assets/Systems/Interaction/Interactable Types/InteractablePickUp.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class InteractablePickUp : MonoBehaviour, IInteractable
 {
     [SerializeField] private string pickupName = "Coin";
+    [SerializeField] private string messageText = "You picked up a coin!";
 
     public void Interact()
     {
+        ServiceHub.Instance.UIManager.ShowMessage(messageText);
+
         Debug.Log($"Picked up {pickupName}");
-        Destroy(gameObject);
+        Destroy(gameObject, 0.1f);
     }
 
     public void OnFocus()
